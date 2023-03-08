cb899ecf16b0785fd52291eacc900d34e2b41a62
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-03-08 14:50:20 +0330

    Add project files.

---
+++ v2ray-Shop-Telegram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace v2ray_Shop_Telegram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
