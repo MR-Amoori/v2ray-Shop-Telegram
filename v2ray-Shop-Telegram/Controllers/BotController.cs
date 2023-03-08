456bc70ca11b6ce4691c857f31e552d96a0ddf21
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-03-08 15:21:17 +0330

    Add Bot Model

---
+++ v2ray-Shop-Telegram/Controllers/BotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace v2ray_Shop_Telegram.Controllers
+{
+    public class BotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ v2ray-Shop-Telegram/Models/BotModel.cs
@@ -0,0 +1,9 @@
+﻿namespace v2ray_Shop_Telegram.Models
+{
+    public class BotModel
+    {
+        public int BotId { get; set; }
+        public string BotToken { get; set; }
+        public string BotName { get; set; }
+    }
+}
