c9e95e98cb82f9eff4d4ede9931a5478ea33b958
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-03-09 13:28:49 +0330

    AccountModel

---
+++ v2ray-Shop-Telegram/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace v2ray_Shop_Telegram.Models
+{
+    public class Account
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public int ChatId { get; set; }
+
+        public float Wallet { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+    }
+}
